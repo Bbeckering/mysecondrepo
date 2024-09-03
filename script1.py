b0ae7c60c07579558cfb23920eb027879705c40d
Bennet Beckering <bennet_beckering1@baylor.edu>
2024-09-03 15:12:07 -0500

    Completed Assignment

--- script1.py
+++ script1.py
@@ -2,15 +2,17 @@
 # Correc the mistakes and produce the following output:
 # Johnny, the total of your purchase is $1,688.70
 
-def calcTotal(amt,tax,name):
-    mytotal = amt * (1+tax)
-    print(f"{myname}, the total of your purchase is ${total:,.2f}")
+def calcTotal(price,thetax,myname):
+    mytotal = price * (1 + thetax)
+    print(f"{myname}, the total of your purchase is ${mytotal:,.2f}")
+
+
 
 myname = 'Johnny'
-price = 1,560
-thetax = 8.25%
+price = 1560
+thetax = 8.25/100
 
-calcTotal(thetax,price,name)
+calcTotal(price,thetax,myname)
 
 
 
@@ -21,6 +23,7 @@ calcTotal(thetax,price,name)
 list1 = [1,'two',3.2,'four',5]
 
 for i in list1:
+   if type(i) == int:
     print(i)
 
 
